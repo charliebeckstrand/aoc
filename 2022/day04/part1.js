ec6b241fa9f02f15b99648ac0358811fe190a680
Charlie Beckstrand <charliebeckstrand@charlies-air.lan>
2022-12-04 01:45:30 -0700

    Added solution for 2022 day 4

---
+++ 2022/day04/part1.js
@@ -0,0 +1,28 @@
+let sections = require('./input')
+
+function getFullOverlap () {
+	let count = 0
+	
+	sections.forEach(section => {		
+		const [range1, range2] = formatSection(section)
+		const [range1start, range1end] = range1
+		const [range2start, range2end] = range2
+		
+		if (
+			(range2start >= range1start && range2end <= range1end)
+			|| (range1start >= range2start && range1end <= range2end)
+		) {
+			count++
+		}
+	})
+	
+	console.log(`Overlap count is ${count}`)
+	
+	return count
+}
+
+function formatSection (section) {
+	return section.split(',').map(n => n.split('-').map(Number))
+}
+
+getFullOverlap()
---
+++ 2022/day04/part2.js
@@ -0,0 +1,28 @@
+let sections = require('./input')
+
+function getOverlap () {
+	let count = 0
+	
+	sections.forEach(section => {		
+		const [range1, range2] = formatSection(section)
+		const [range1start, range1end] = range1
+		const [range2start, range2end] = range2
+		
+		if (
+			range2end >= range1start 
+			&& range2start <= range1end
+		) {
+			count++
+		}
+	})
+	
+	console.log(`Overlap count is ${count}`)
+	
+	return count
+}
+
+function formatSection (section) {
+	return section.split(',').map(n => n.split('-').map(Number))
+}
+
+getOverlap()
