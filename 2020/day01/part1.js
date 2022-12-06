58a1d6c5aeb7f090e1b1d2d2e50077fa5c125612
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2022-12-06 01:19:40 -0700

    Improved 2020 day 1 solutions

--- 2020/day01/part1.js
+++ 2020/day01/part1.js
@@ -1,14 +1,10 @@
 const input = require('./input')
 
 function findProduct(numbers) {
-	// Loop through each number in the array
 	for (let i = 0; i < numbers.length; i++) {
-		// Loop through the remaining numbers in the array (starting at the next index)
 		for (let j = i + 1; j < numbers.length; j++) {
-			// Check if the current numbers sum to 2020
 			if (numbers[i] + numbers[j] === 2020) {
 				const product = numbers[i] * numbers[j]
-				// Return the product of the two numbers
 				return product
 			}
 		}
--- 2020/day01/part2.js
+++ 2020/day01/part2.js
@@ -1,16 +1,11 @@
 const input = require('./input')
 
 function findProduct(numbers) {
-	// Loop through each number in the array
 	for (let i = 0; i < numbers.length; i++) {
-		// Loop through the remaining numbers in the array (starting at the next index)
 		for (let j = i + 1; j < numbers.length; j++) {
-			// Loop through the remaining numbers in the array (starting at the next index)
 			for (let k = j + 1; k < numbers.length; k++) {
-				// Check if the current numbers sum to 2020
 				if (numbers[i] + numbers[j] + numbers[k] === 2020) {
 					const product = numbers[i] * numbers[j] * numbers[k]
-					// Return the product of the three numbers
 					return product
 				}
 			}
