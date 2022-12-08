7abd636685888781e03049d73924ba85d6cf7ff5
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2022-12-08 09:19:06 -0700

    Improved 2022 day 8 part 1 solution

--- 2022/day08/part1.js
+++ 2022/day08/part1.js
@@ -1,31 +1,23 @@
 const grid = require('./input')
 
-function countVisibleTrees (grid) {
+function countVisibleTrees () {
 	let count = 0
 		
 	for (let i = 0; i < grid.length; i++) {		
-		for (let j = 0; j < grid[i].length; j++) {			
-			let [edge, up, down, left, right] = []
-			
-			edge = visibleEdge(grid, i, j)
-			up = visibleFromUp(grid, i, j)
-			down = visibleFromDown(grid, i, j)
-			left = visibleFromLeft(grid, i, j)
-			right = visibleFromRight(grid, i, j)
-			
-			if ([edge, up, down, left, right].includes(true)) { count++ }
+		for (let j = 0; j < grid[i].length; j++) {
+			if (visible(i, j).includes(true)) { count++ }
 		}
 	}
 	
 	return count
 }
 
-const count = countVisibleTrees(grid)
+const count = countVisibleTrees()
 
 console.log(count)
 
-function visibleEdge (grid, i, j) {
-	let visible = true
+function visible (i, j) {
+	let [edge, up, down, left, right] = [true, true, true, true, true]
 	
 	if (
 		i !== 0 || 
@@ -33,60 +25,36 @@ function visibleEdge (grid, i, j) {
 		i !== grid.length - 1 ||
 		j !== grid[i].length - 1
 	) {
-		visible = false
+		edge = false
 	}
-	
-	return visible
-}
-
-function visibleFromUp (grid, i, j) {
-	let visible = true
 	
 	for (let k = 0; k < j; k++) {
 		if (grid[k][i] >= grid[j][i]) {
-			visible = false
+			up = false
 			break
 		}
 	}
-	
-	return visible
-}
-
-function visibleFromDown (grid, i, j) {
-	let visible = true
 	
 	for (let k = grid.length - 1; k > j; k--) {
 		if (grid[k][i] >= grid[j][i]) {
-			visible = false
+			down = false
 			break
 		}
 	}
-	
-	return visible
-}
-
-function visibleFromLeft (grid, i, j) {
-	let visible = true
 	
 	for (let k = 0; k < i; k++) {
 		if (grid[j][k] >= grid[j][i]) {
-			visible = false
+			left = false
 			break
 		}
 	}
-	
-	return visible
-}
-
-function visibleFromRight (grid, i, j) {
-	let visible = true
 	
 	for (let k = grid.length - 1; k > i; k--) {
 		if (grid[j][k] >= grid[j][i]) {
-			visible = false
+			right = false
 			break
 		}
 	}
 	
-	return visible
+	return [edge, up, down, left, right]
 }
