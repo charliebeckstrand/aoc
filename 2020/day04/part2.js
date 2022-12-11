2395aa9762f198167971763174276171dd5590a7
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2022-12-11 09:10:55 -0700

    Whitespace improvements to 2020 day 4 part 2

--- 2020/day04/part2.js
+++ 2020/day04/part2.js
@@ -4,25 +4,33 @@ function countValidPassports (passports) {
 	const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 	
 	const fieldValidators = {
-		byr: value => {
+		byr: value => {			
 			if (!/^\d{4}$/.test(value)) return false
+			
 			const year = parseInt(value, 10)
+
 			return year >= 1920 && year <= 2002
 		},
 		iyr: value => {
 			if (!/^\d{4}$/.test(value)) return false
+
 			const year = parseInt(value, 10)
+			
 			return year >= 2010 && year <= 2020
 		},
 		eyr: value => {
 			if (!/^\d{4}$/.test(value)) return false
+
 			const year = parseInt(value, 10)
+			
 			return year >= 2020 && year <= 2030
 		},
 		hgt: value => {
 			if (!/^\d+(cm|in)$/.test(value)) return false
+
 			const number = parseInt(value, 10)
 			const unit = value.substring(value.length - 2)
+			
 			if (unit === 'cm') return number >= 150 && number <= 193
 			if (unit === 'in') return number >= 59 && number <= 76
 			return false
