0aaf35f54ef1440e0624bc2254d903ab06964439
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2024-12-03 10:24:07 -0700

    Improved comments for 2024 day 1

--- 2024/day01/part1.js
+++ 2024/day01/part1.js
@@ -2,21 +2,21 @@ import lists from './input.js'
 
 const { left, right } = { left: [], right: [] }
 
-// split the lists
+// Split the lists
 lists.split('\n').forEach((line) => {
 	const [l, r] = line.split('   ')
 	left.push(+l)
 	right.push(+r)
 })
 
-// sort the lists
+// Sort the lists
 left.sort((a, b) => a - b)
 right.sort((a, b) => a - b)
 
-// get the differences
+// Get the differences
 const differences = left.map((l, i) => Math.abs(l - right[i]))
 
-// get the sum of the differences
+// Get the sum of the differences
 const sum = differences.reduce((a, b) => a + b, 0)
 
 console.log(sum)
--- 2024/day01/part2.js
+++ 2024/day01/part2.js
@@ -2,22 +2,22 @@ import lists from './input.js'
 
 const { left, right } = { left: [], right: [] }
 
-// split the lists
+// Split the lists
 lists.split('\n').forEach((line) => {
 	const [l, r] = line.split('   ')
 	left.push(+l)
 	right.push(+r)
 })
 
-// sort the lists
+// Sort the lists
 left.sort((a, b) => a - b)
 right.sort((a, b) => a - b)
 
 /*
-    check how many times each element in the left list appears in the right list
-    then multiply by that value
-    then sum all the values
-*/
+ * Check how many times each element in the left list appears in the right list
+ * Multiply by that value
+ * Sum all the values
+ */
 const similarity = left.reduce((acc, l) => acc + right.filter((r) => r === l).length * l, 0)
 
 console.log(similarity)
