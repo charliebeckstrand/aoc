0385b467c9e21cd59b5ad984c2eca130afe876e9
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2024-12-03 10:07:19 -0700

    improved 2024 day 3 solutions

--- 2024/day03/part1.js
+++ 2024/day03/part1.js
@@ -2,16 +2,16 @@ import program from './input.js'
 
 const extractMuls = () => {
 	// Find all instances of 'mul(x, y)' in the program
-	let muls = program.match(/mul\((\d+),(\d+)\)/g) || []
+	let tokens = program.match(/mul\((\d+),(\d+)\)/g) || []
 
 	// Extract the two numbers from each instance
-	muls = muls.map((mul) => mul.match(/mul\((\d+),(\d+)\)/).slice(1))
+	tokens = tokens.map((token) => token.match(/mul\((\d+),(\d+)\)/).slice(1))
 
 	// Multiply the two numbers
-	muls = muls.map((mul) => mul[0] * mul[1])
+	tokens = tokens.map((token) => token[0] * token[1])
 
 	// Sum the results
-	const result = muls.reduce((acc, val) => acc + val, 0)
+	const result = tokens.reduce((acc, val) => acc + val, 0)
 
 	return result
 }
