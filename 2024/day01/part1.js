ad06a53c20928f7553b1ad0f0fd1029de69a3719
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2024-12-05 12:17:45 -0700

    improved 2024 comments

--- 2024/day01/part1.js
+++ 2024/day01/part1.js
@@ -3,23 +3,23 @@ import lists from './input.js'
 const left = []
 const right = []
 
+// Split the lists
+lists.split('\n').forEach((line) => {
+	const [l, r] = line.split('   ')
+	left.push(+l)
+	right.push(+r)
+})
+
+// Sort the lists
+left.sort((a, b) => a - b)
+right.sort((a, b) => a - b)
+
 /*
  * Get the sum of the differences between the lists.
- * 1. Split the lists
- * 2. Sort the lists
- * 3. Get the differences
- * 4. Get the sum of the differences
+ * 1. Get the differences
+ * 2. Return the sum of the differences
  */
 const getDifferences = () => {
-	lists.split('\n').forEach((line) => {
-		const [l, r] = line.split('   ')
-		left.push(+l)
-		right.push(+r)
-	})
-
-	left.sort((a, b) => a - b)
-	right.sort((a, b) => a - b)
-
 	const differences = left.map((l, i) => Math.abs(l - right[i]))
 
 	return differences.reduce((a, b) => a + b, 0)
--- 2024/day01/part2.js
+++ 2024/day01/part2.js
@@ -3,26 +3,26 @@ import lists from './input.js'
 const left = []
 const right = []
 
+// Split the lists
+lists.split('\n').forEach((line) => {
+	const [l, r] = line.split('   ')
+	left.push(+l)
+	right.push(+r)
+})
+
+// Sort the lists
+left.sort((a, b) => a - b)
+right.sort((a, b) => a - b)
+
 /*
  * Get the sum of the products of the similarities between the lists.
- * 1. Split the lists
- * 2. Sort the lists
- * 3. Check how many times each element in the left list appears in the right list
- * 4. Return the sum of the products
+ * 1. Check how many times each element in the left list appears in the right list
+ * 2. Return the sum of the products
  */
 const getSimilarity = () => {
-	lists.split('\n').forEach((line) => {
-		const [l, r] = line.split('   ')
-		left.push(+l)
-		right.push(+r)
-	})
-
-	left.sort((a, b) => a - b)
-	right.sort((a, b) => a - b)
-
 	const similarity = left.reduce((acc, l) => acc + right.filter((r) => r === l).length * l, 0)
 
-	console.log(similarity)
+	return similarity
 }
 
 console.log(getSimilarity())
