60b901324c54f50d49e78cf9f6557a03d146526b
Charlie Beckstrand <charliebeckstrand@Charlies-MacBook-Air.local>
2025-12-03 12:07:06 -0700

    Added solutions for 2025 day 2

---
+++ 2025/day02/part2.js
@@ -0,0 +1,19 @@
+import ids from './input.js'
+
+const sumInvalidIds = (ids) => {
+	function* generateIds() {
+		for (const id of ids) {
+			const [first, last] = id.split('-').map(Number)
+
+			for (let i = first; i <= last; i++) {
+				yield i
+			}
+		}
+	}
+
+	return Array.from(generateIds())
+		.filter((num) => /^(.+)\1+$/.test(num))
+		.reduce((sum, num) => sum + num, 0)
+}
+
+console.log(sumInvalidIds(ids))
